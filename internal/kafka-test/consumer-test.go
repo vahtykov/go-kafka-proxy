0e84e9579597dde50a0f8b016c1e3b427355348c
Ilya Vahtykov <ilya.v@profi.io>
2025-01-22 17:42:25 +0300

    test consumer added

--- internal/kafka-test/consumer-test.go
+++ internal/kafka-test/consumer-test.go
@@ -1,134 +1,59 @@
-package kafka_test
+package main
 
 import (
 	"context"
-	"errors"
+	"fmt"
 	"log"
-	"strings"
-	"sync"
 
 	"github.com/IBM/sarama"
-	"gorm.io/gorm"
 )
 
-func NewConsumerTest(
-	brokers string,
-	topic string,
-	groupID string,
-	db *gorm.DB,
-	serviceURL string,
-) (*Consumer, error) {
-	log.Println("Starting a new Sarama consumer")
+func main() {
+    config := sarama.NewConfig()
+    config.Consumer.Return.Errors = true
+    
+    // Создаем клиента
+    brokers := []string{"localhost:9092"}
+    client, err := sarama.NewClient(brokers, config)
+    if err != nil {
+        log.Fatalf("Error creating client: %v", err)
+    }
+    defer client.Close()
 
-	config := sarama.NewConfig()
+    // Создаем consumer
+    consumer, err := sarama.NewConsumerFromClient(client)
+    if err != nil {
+        log.Fatalf("Error creating consumer: %v", err)
+    }
+    defer consumer.Close()
 
-	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
-	config.Consumer.Offsets.Initial = sarama.OffsetOldest
+    // Указываем топик и партицию
+    topic := "plans"
+    partition := int32(0)
+    
+    // Получаем партицию
+    partitionConsumer, err := consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+    if err != nil {
+        log.Fatalf("Error creating partition consumer: %v", err)
+    }
+    defer partitionConsumer.Close()
 
-	/**
-	 * Setup a new Sarama consumer group
-	 */
-	consumer := Consumer{
-		ready: make(chan bool),
-	}
+    // Создаем контекст с отменой
+    ctx, cancel := context.WithCancel(context.Background())
+    defer cancel()
 
-	ctx, cancel := context.WithCancel(context.Background())
-	client, err := sarama.NewConsumerGroup(strings.Split(brokers, ","), groupID, config)
-	if err != nil {
-		log.Panicf("Error creating consumer group client: %v", err)
-	}
+    // Читаем сообщения
+    for {
+        select {
+        case msg := <-partitionConsumer.Messages():
+            fmt.Printf("Message topic:%s partition:%d offset:%d\n value:%s\n",
+                msg.Topic, msg.Partition, msg.Offset, string(msg.Value))
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
+        case err := <-partitionConsumer.Errors():
+            fmt.Printf("Error: %v\n", err)
 
-	topics := []string{topic}
-
-	go func() {
-		defer wg.Done()
-
-		for {
-			// `Consume` should be called inside an infinite loop, when a
-			// server-side rebalance happens, the consumer session will need to be
-			// recreated to get the new claims
-			if err := client.Consume(ctx, topics, &consumer); err != nil {
-				if errors.Is(err, sarama.ErrClosedConsumerGroup) {
-					return
-				}
-				log.Panicf("Error from consumer: %v", err)
-			}
-			// check if context was cancelled, signaling that the consumer should stop
-			if ctx.Err() != nil {
-				return
-			}
-			consumer.ready = make(chan bool)
-		}
-	}()
-
-	<-consumer.ready // Await till the consumer has been set up
-	log.Println("Sarama consumer up and running!...")
-
-	cancel()
-	wg.Wait()
-	// if err = client.Close(); err != nil {
-	// 	log.Panicf("Error closing client: %v", err)
-	// }
-
-	return &consumer, nil
-}
-
-// Consumer represents a Sarama consumer group consumer
-type Consumer struct {
-	ready chan bool
-}
-
-// Setup is run at the beginning of a new session, before ConsumeClaim
-func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
-	// Mark the consumer as ready
-	close(consumer.ready)
-	return nil
-}
-
-// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
-func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
-	return nil
-}
-
-func toggleConsumptionFlow(client sarama.ConsumerGroup, isPaused *bool) {
-	if *isPaused {
-		client.ResumeAll()
-		log.Println("Resuming consumption")
-	} else {
-		client.PauseAll()
-		log.Println("Pausing consumption")
-	}
-
-	*isPaused = !*isPaused
-}
-
-// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
-// Once the Messages() channel is closed, the Handler must finish its processing
-// loop and exit.
-func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	// NOTE:
-	// Do not move the code below to a goroutine.
-	// The `ConsumeClaim` itself is called within a goroutine, see:
-	// https://github.com/IBM/sarama/blob/main/consumer_group.go#L27-L29
-
-	for {
-		select {
-			case message, ok := <-claim.Messages():
-				if !ok {
-					log.Printf("message channel was closed")
-					return nil
-				}
-				log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
-				session.MarkMessage(message, "")
-
-			// Should return when `session.Context()` is done.
-			// If not, will raise `ErrRebalanceInProgress` or `read tcp <ip>:<port>: i/o timeout` when kafka rebalance. see:
-			// https://github.com/IBM/sarama/issues/1192
-			case <-session.Context().Done():
-				return nil
-		}
-	}
+        case <-ctx.Done():
+            return
+        }
+    }
 }
